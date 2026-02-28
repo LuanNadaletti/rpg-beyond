ae95be6663476215ac0beb76c9bb37d841a11932
Guilherme Pereira <guilhermepereira2917@gmail.com>
2026-02-28 18:32:36 -0300

    add frontend project

---
+++ myconid/src/app/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import './index.css';
+import App from './App';
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+      <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ myconid/src/components/header.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react"
+
+export default function Header(): ReactNode {
+    return (
+        <header>RPG Beyond</header>
+    );
+}
